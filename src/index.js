9a3e794150869177a78735fa03ade2009de76055
Hoarau <hoarau.kevin@whatafix.com>
2021-07-28 12:22:54 +0200

    vue3
    changes on main.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import Datetime from './Datetime.vue'
 import DatetimePopup from './DatetimePopup.vue'
-
-function plugin (Vue) {
-  Vue.component('datetime', Datetime)
-  Vue.component('datetime-popup', DatetimePopup)
-}
-
-// Install by default if using the script tag
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(plugin)
-}
-
-export default plugin
 const version = '__VERSION__'
 
-// Export all components too
+export default {
+  install: (app, options) => {
+    app.component('datetime', Datetime)
+    app.component('datetime-popup', DatetimePopup)
+  }
+}
+
 export {
-  Datetime,
-  DatetimePopup,
   version
 }
